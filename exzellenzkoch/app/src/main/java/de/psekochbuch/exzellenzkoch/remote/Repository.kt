a80ef1a17cfa467c81205ab81716470eeed0aa0b
Pascal Maier <paas_m@t-online.de>
2020-01-21 18:58:24 +0100

    Add retrofit

--- exzellenzkoch/app/src/main/java/de/psekochbuch/exzellenzkoch/remote/Repository.kt
+++ exzellenzkoch/app/src/main/java/de/psekochbuch/exzellenzkoch/remote/Repository.kt
@@ -1,25 +1,60 @@
 package de.psekochbuch.exzellenzkoch.remote
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
-class Repository(token:String)
+class Repository(firebaseToken:String?)
 {
     var retrofit:Retrofit? = null
+    private val BASE_URL = "http://193.196.38.185:8080/api/"
+    private val token = firebaseToken
 
     init
     {
+        val gson : Gson = GsonBuilder().setLenient().create()
 
+        retrofit = Retrofit.Builder().baseUrl(BASE_URL).client(createHttpClient()).addConverterFactory(
+            GsonConverterFactory.create(gson)).build()
     }
 
-    private fun createHttpClient() : OkHttpClient?
+    fun createHttpClient() : OkHttpClient
     {
-        return null
+        //var token = uss?.getIdToken(false)?.result?.token
+        if(token != null) {
+            return OkHttpClient().newBuilder().addInterceptor(object : Interceptor {
+                @Throws(IOException::class)
+                override fun intercept(chain: Interceptor.Chain): okhttp3.Response? {
+                    val originalRequest: Request = chain.request()
+                    val builder: Request.Builder = originalRequest.newBuilder().header(
+                        "Authorization", token
+                    )
+                    val newRequest: Request = builder.build()
+                    return chain.proceed(newRequest)
+                }
+            }).build()
+        }
+        return OkHttpClient().newBuilder().addInterceptor(object : Interceptor {
+            @Throws(IOException::class)
+            override fun intercept(chain: Interceptor.Chain): okhttp3.Response? {
+                val originalRequest: Request = chain.request()
+                val builder: Request.Builder = originalRequest.newBuilder().header(
+                    "Authorization", token
+                )
+                val newRequest: Request = builder.build()
+                return chain.proceed(newRequest)
+            }
+        }).build()
     }
 
-    fun createApi(api :Class<out Any>)
+    fun createApi(api :Class<out Any>) : Any
     {
-
+        return retrofit!!.create(api)
     }
 
 
