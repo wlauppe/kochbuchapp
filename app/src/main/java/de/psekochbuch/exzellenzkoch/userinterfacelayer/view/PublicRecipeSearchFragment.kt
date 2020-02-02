0d2a5ab4bd5ba6545f13a92b877ffb10d4382210
lalanator <lea.strauch@gmx.de>
2020-02-02 19:11:17 +0100

    fixed build

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/view/PublicRecipeSearchFragment.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/view/PublicRecipeSearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -24,14 +25,14 @@ class PublicRecipeSearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //binding set to the according Fragment
-        binding = PublicRecipeSearchFragmentBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.public_recipe_search_fragment, container, false)
         //viewmodel recieved by viewmodelproviders
         viewModel = ViewModelProvider(this).get(PublicRecipeSearchViewmodel::class.java)
         //Sets according viewmodel from XML to this fragment
         binding.publicRecipeSearchViewModel = viewModel
         //initialized navcontoller
         var navController: NavController = findNavController()
-        binding.searchButton.setOnClickListener {
+        binding.buttonSearchRecipeSearch.setOnClickListener {
             navController.navigate(R.id.action_publicRecipeSearchFragment_to_displaySearchListFragment)
         }
         return binding.root
