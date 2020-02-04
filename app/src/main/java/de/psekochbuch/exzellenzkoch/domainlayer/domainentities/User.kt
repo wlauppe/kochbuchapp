26c2c379336a2cdddb7b4ea332ba5d299cef813b
MagnumMandel <uxnft@student.kit.edu>
2020-02-04 17:30:43 +0100

    Die Ruhe vor dem Sturm

--- app/src/main/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/User.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/User.kt
@@ -3,46 +3,14 @@ package de.psekochbuch.exzellenzkoch.domainlayer.domainentities
 import android.os.Parcel
 import android.os.Parcelable
 
-class User() : Parcelable{
+class User(id:String, img: String, desc :String) {
 
     var isAdmin = false
 
-    var userID :String? = null
+    var userID :String? = id
 
-    var img : String? = null
+    var img : String? = img
 
-    var description: String? = null
-
-    constructor(parcel: Parcel) : this() {
-        isAdmin = parcel.readByte() != 0.toByte()
-        userID = parcel.readString()
-        img = parcel.readString()
-        description = parcel.readString()
-    }
-
-
-
-    //PArceble for safeargs
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeByte(if (isAdmin) 1 else 0)
-        parcel.writeString(userID)
-        parcel.writeString(img)
-        parcel.writeString(description)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
-        }
-
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
-        }
-    }
-
+    var description: String? = desc
 
 }
--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ProfileDisplayViewmodel.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ProfileDisplayViewmodel.kt
@@ -1,15 +1,20 @@
 package de.psekochbuch.exzellenzkoch.userinterfacelayer.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import de.psekochbuch.exzellenzkoch.datalayer.interfaceimplementation.serviceimplementations.PublicRecipeFakeRepository
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.PublicRecipe
+import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.User
 
 class ProfileDisplayViewmodel : ViewModel() {
 
     var repo = PublicRecipeFakeRepository()
-    var recipes: LiveData<MutableList<PublicRecipe>> =
-        repo.getPublicRecipes() as LiveData<MutableList<PublicRecipe>>
+    var recipes: LiveData<List<PublicRecipe>> =repo.getPublicRecipes()
+
+    var users : LiveData<List<User>> = repo.getUsers()
+
+
 
 
     fun isOwner(): Boolean {
