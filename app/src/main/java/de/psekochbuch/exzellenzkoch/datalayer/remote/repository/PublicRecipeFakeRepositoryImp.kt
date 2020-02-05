3ab29088b75b198108abfc25d20b812b6f48b26f
wolf <whlauppe@web.de>
2020-02-05 19:24:54 +0100

    weitere mergefehler korrigiert

--- app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/repository/PublicRecipeFakeRepositoryImp.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/repository/PublicRecipeFakeRepositoryImp.kt
@@ -3,7 +3,6 @@ package de.psekochbuch.exzellenzkoch.datalayer.remote.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.PublicRecipe
-import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.PublicRecipeFakeRepository
 import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.PublicRecipeRepository
 
 
--- app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/repository/UserRepositoryImp.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/repository/UserRepositoryImp.kt
@@ -1,11 +1,13 @@
 package de.psekochbuch.exzellenzkoch.datalayer.remote.repository
 
+import androidx.lifecycle.LiveData
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.User
 import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.UserRepository
 
 class UserRepositoryImp : UserRepository  {
-    override suspend fun getUser(id: String): User {
+    override fun getUsers(): LiveData<List<User>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 
 }
