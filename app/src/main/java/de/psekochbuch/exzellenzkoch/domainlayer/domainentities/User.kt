898fa770c8ae7f2dba7e5488f5c5467fba9b0c99
wolf <whlauppe@web.de>
2020-02-05 19:35:57 +0100

    user ueberarbeitet

--- app/src/main/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/User.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/User.kt
@@ -3,14 +3,9 @@ package de.psekochbuch.exzellenzkoch.domainlayer.domainentities
 import android.os.Parcel
 import android.os.Parcelable
 
-class User(id:String, img: String, desc :String) {
+class User(id:String, var img: String = "", var desc :String = "") {
 
     var isAdmin = false
 
-    var userID :String? = id
-
-    var img : String? = img
-
-    var description: String? = desc
 
 }
