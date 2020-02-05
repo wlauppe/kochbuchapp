54942f0fdce7269198ed14e8a47e49770ccb30ff
wolf <whlauppe@web.de>
2020-02-05 19:47:01 +0100

    Fix Admin Adapter

--- app/src/main/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/User.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/User.kt
@@ -3,7 +3,7 @@ package de.psekochbuch.exzellenzkoch.domainlayer.domainentities
 import android.os.Parcel
 import android.os.Parcelable
 
-class User(id:String, var img: String = "", var desc :String = "") {
+class User(val userId:String, var img: String = "", var desc :String = "") {
 
     var isAdmin = false
 
