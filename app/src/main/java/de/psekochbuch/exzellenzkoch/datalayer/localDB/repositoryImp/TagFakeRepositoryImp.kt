11b10fcff8be7d3b7846cd84ff244879c8e8901e
wolf <whlauppe@web.de>
2020-02-07 11:22:41 +0100

    fake repositories added

---
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/localDB/repositoryImp/TagFakeRepositoryImp.kt
@@ -0,0 +1,10 @@
+package de.psekochbuch.exzellenzkoch.datalayer.localDB.repositoryImp
+
+import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.TagList
+import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.TagRepository
+
+class TagFakeRepositoryImp : TagRepository {
+    override fun getTags(): TagList {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
