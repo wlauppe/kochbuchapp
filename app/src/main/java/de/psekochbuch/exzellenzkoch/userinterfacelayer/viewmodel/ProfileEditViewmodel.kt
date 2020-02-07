8496a53ad0b77e742a689bca980e074746caf8bc
Magnus Bühler <Magnus.b.buehler@gmail.com>
2020-02-07 15:29:33 +0100

    ProfileEdit Fragment mit Databinding und Glide implementiert

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ProfileEditViewmodel.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ProfileEditViewmodel.kt
@@ -1,11 +1,32 @@
 package de.psekochbuch.exzellenzkoch.userinterfacelayer.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.UserFakeRepositoryImp
+import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.User
 
 class ProfileEditViewmodel : ViewModel() {
+    var user : LiveData<User> = UserFakeRepositoryImp().getUserByID("")
+
+        //LiveData
+        var userID : LiveData<String> = MutableLiveData("")
+        var userDesc : LiveData<String> = MutableLiveData("")
+        var userImgURL : LiveData<String> = MutableLiveData("")
 
 
-    fun deleteUser() {
+    fun deleteUser(id: String) {
+
+
+    }
+
+    fun setUserByID(id:String){
+        var user = UserFakeRepositoryImp().getUserByID(id)
+
+        this.user = user
+        this.userID = MutableLiveData(user.value!!.userId)
+        this.userDesc = MutableLiveData(user.value!!.desc)
+        this.userImgURL = MutableLiveData(user.value!!.img)
 
     }
 }
