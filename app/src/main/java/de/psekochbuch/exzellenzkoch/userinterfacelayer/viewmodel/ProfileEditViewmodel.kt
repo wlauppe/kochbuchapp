86da153b734b1b01eeed0a79b8fbac8a55f29b9c
MagnumMandel <uxnft@student.kit.edu>
2020-02-08 11:30:06 +0100

    ProfileEditVeiwmodel angepasst

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ProfileEditViewmodel.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ProfileEditViewmodel.kt
@@ -15,20 +15,37 @@ class ProfileEditViewmodel : ViewModel() {
         var userImgURL : LiveData<String> = MutableLiveData("")
 
 
+    /**
+     * Removes the user from the database on the server. The Logindata gets resetted and the profil
+     * can no longer be accessed
+     * @param id: The userId which connects to the user
+     */
     fun deleteUser(id: String) {
 
 
     }
 
+    /**
+     * Gets the User which is connected with the id. Checks if the User exists and if the user is
+     * valid it sets the LiveData from the viewmodel with the data.
+     * @param id:
+     */
     fun setUserByID(id:String){
         var user = UserFakeRepositoryImp().getUser(id)
+        if(user.value!!.userId == ""){
 
+        }
         this.user = user
         this.userID = MutableLiveData(user.value!!.userId)
         this.userDesc = MutableLiveData(user.value!!.description)
         this.userImgURL = MutableLiveData(user.value!!.imgUrl)
+    }
+
+    fun changeLoginData(){
+
 
     }
+
 }
 
 
