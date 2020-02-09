9e68aa1e339e8bf10923a2a2a84b09907b1c8380
Thomas Weidmann <T.A.Weidmann@web.de>
2020-02-10 00:21:32 +0100

    etwas in UserRepositoryImp

--- app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/repository/UserRepositoryImp.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/repository/UserRepositoryImp.kt
@@ -1,10 +1,22 @@
 package de.psekochbuch.exzellenzkoch.datalayer.remote.repository
 
 import androidx.lifecycle.LiveData
+import de.psekochbuch.exzellenzkoch.datalayer.remote.ApiServiceBuilder
+import de.psekochbuch.exzellenzkoch.datalayer.remote.api.PublicRecipeApi
+import de.psekochbuch.exzellenzkoch.datalayer.remote.api.UserApi
+import de.psekochbuch.exzellenzkoch.datalayer.remote.mapper.PublicRecipeDtoEntityMapper
+import de.psekochbuch.exzellenzkoch.datalayer.remote.mapper.UserDtoEntityMapper
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.User
 import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.UserRepository
 
 class UserRepositoryImp : UserRepository  {
+    val recipeMapper = PublicRecipeDtoEntityMapper()
+    val userMapper = UserDtoEntityMapper()
+
+    val token = null
+    //TODO token von Authentification Interface bekommen.
+
+
     override fun getUsers(): LiveData<List<User>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
