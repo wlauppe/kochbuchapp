372856d7d425a0c3cd2abd2128baf343e69a2477
PassM <pascal.maier2@student.kit.edu>
2020-02-10 03:01:19 +0100

    add registration function in viewmodel

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/RegistrationViewModel.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/RegistrationViewModel.kt
@@ -2,14 +2,20 @@ package de.psekochbuch.exzellenzkoch.userinterfacelayer.viewmodel
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.UserRepositoryImp
 import de.psekochbuch.exzellenzkoch.datalayer.remote.service.AuthentificationImpl
+import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.User
+import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.UserRepository
 
 
 import de.psekochbuch.exzellenzkoch.userinterfacelayer.AuthenticationResult
+import kotlinx.coroutines.launch
 
-class RegistrationViewModel : ViewModel() {
+class RegistrationViewModel() : ViewModel() {
 
 
     var email: MutableLiveData<String> = MutableLiveData()
@@ -19,32 +25,59 @@ class RegistrationViewModel : ViewModel() {
 
     fun registerOnClick() {
 
+        val userRepository = UserRepositoryImp()
 
-        if(userId.value != ""){
-            //Repo anfrage, ob ID schon existiert
-            //-> 5 Sekunden Wartezeit für Serverantwort
-            //boolean abfrage der SErverantwort
+        progressBarVisibility.postValue(true)
+        val id = userId.value
+        if(id != "" && id != null){
+            viewModelScope.launch {
+                try {
+                    val user : User? = userRepository.checkUser(id)
+                    if(user?.userId == "") {
+
+                        val em = email.value
+                        val pw = password.value
+                        if (em != null && pw != null) {
+                            AuthentificationImpl
+                                .register(em, pw, id) { it, result ->
+                                    if (it != null && result == AuthenticationResult.REGISTRATIONSUCCESS) {
+                                        Log.d(TAG, "Registration erfolgreich")
+                                        userRepository.setToken(it)
+                                        viewModelScope.launch {
+                                            try {
+                                                userRepository.addUser(id)
+                                            }catch (e: Exception)
+                                            {
+
+                                            }
+
+                                            progressBarVisibility.postValue(false)
+                                        }
+                                    } else {
+                                        progressBarVisibility.postValue(false)
+                                    }
+                                }
+                        }
+                        progressBarVisibility.postValue(false)
+                    }
+
+                }catch (ex: Exception)
+                {
+                    ex.printStackTrace()
+                }
+                finally {
+                    progressBarVisibility.postValue(false)
+                }
+                //Repo anfrage, ob ID schon existiert
+                //-> 5 Sekunden Wartezeit für Serverantwort
+                //boolean abfrage der SErverantwort
                 //-> falls ja : Toast ausgabe und return
                 //-> falls nein: unterer code
+            }
 
         }
         //email.postValue("bal")
-        progressBarVisibility.postValue(true)
-        val em = email.value
-        val pw = password.value
-        if (em != null && pw != null) {
-            AuthentificationImpl()
-                .register(em, pw) { it, result ->
-                    if (it != null && result == AuthenticationResult.REGISTRATIONSUCCESS) {
-                        Log.d(TAG, "Registration erfolgreich")
 
-                        progressBarVisibility.postValue(false)
-                    } else {
-                        progressBarVisibility.postValue(false)
-                    }
-                }
-        }
-        progressBarVisibility.postValue(false)
     }
 
     fun registrationSuccess():Boolean{
