7245722dcb9d8ef02d097571d592cea39fe14acc
MagnumMandel <uxnft@student.kit.edu>
2020-02-10 17:20:36 +0100

    RecipeDisplayViewmodel dicker catchblock

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/RecipeDisplayViewmodel.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/RecipeDisplayViewmodel.kt
@@ -3,10 +3,13 @@ package de.psekochbuch.exzellenzkoch.userinterfacelayer.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.PublicRecipeFakeRepositoryImp
 import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.PublicRecipeRepositoryImp
+import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.PrivateRecipe
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.PublicRecipe
 import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.repository.PublicRecipeRepository
+import kotlinx.coroutines.launch
 import java.util.*
 
 class RecipeDisplayViewmodel(repository:PublicRecipeRepository) : ViewModel() {
@@ -33,6 +36,21 @@ class RecipeDisplayViewmodel(repository:PublicRecipeRepository) : ViewModel() {
     //Attributes
     var id = 0
 
+    /*
+* This variable is private because we don't want to expose MutableLiveData
+*
+* MutableLiveData allows anyone to set a value, and MainViewModel is the only
+* class that should be setting values.
+*/
+
+    private val _errorLiveDataString = MutableLiveData<String?>()
+    /**
+     * Request a snackbar to display a string.
+     */
+    val errorLiveDataString: LiveData<String?>
+        get() = _errorLiveDataString
+
+
     var ingredients = getIngredientsStrings()
 
 
@@ -56,19 +74,22 @@ class RecipeDisplayViewmodel(repository:PublicRecipeRepository) : ViewModel() {
     }
     this.id = id
 
-   var recipe = repo.getPublicRecipe(id)
-
-    if(recipe.value == null){
-        return
-    }
-        image = MutableLiveData(recipe.value!!.imgUrl)
-        title = MutableLiveData(recipe.value!!.title)
-        preparationDescription = MutableLiveData(recipe.value!!.preparation)
-        ingredientChapter = MutableLiveData(recipe.value!!.ingredientChapter)
-        tagsList = MutableLiveData(recipe.value!!.tags)
-        recipeCookTime =MutableLiveData(recipe.value!!.cookingTime)
-        recipePrepTime = MutableLiveData(recipe.value!!.preparationTime)
-        creationTime = MutableLiveData(recipe.value!!.creationTimeStamp)
+        var recipe: LiveData<PublicRecipe> = MutableLiveData()
+        viewModelScope.launch {
+            try {
+                recipe = repo.getPublicRecipe(id)
+                image = MutableLiveData(recipe.value!!.imgUrl)
+                title = MutableLiveData(recipe.value!!.title)
+                preparationDescription = MutableLiveData(recipe.value!!.preparation)
+                ingredientChapter = MutableLiveData(recipe.value!!.ingredientChapter)
+                tagsList = MutableLiveData(recipe.value!!.tags)
+                recipeCookTime =MutableLiveData(recipe.value!!.cookingTime)
+                recipePrepTime = MutableLiveData(recipe.value!!.preparationTime)
+                creationTime = MutableLiveData(recipe.value!!.creationTimeStamp)
+            } catch (error: Error) {
+                _errorLiveDataString.value = error.message
+            }
+        }
     }
 
 //Wunschkriterium
