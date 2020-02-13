5c86f09d1cefc8178db2443f857975fcd07dbf5d
Thomas Weidmann <T.A.Weidmann@web.de>
2020-02-13 17:18:22 +0100

    entitymapper schöner

--- app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/mapper/EntityMapper.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/mapper/EntityMapper.kt
@@ -4,14 +4,34 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 
 abstract class EntityMapper<Entity,Dto> {
+
     abstract fun toEntity(dto: Dto) : Entity
+
     abstract fun toDto(entitiy: Entity) : Dto
-    fun toListEntity(dto: List<Dto>) : List<Entity> = dto.map(::toEntity)
-    fun toListDto(entity: List<Entity>) : List<Dto> = entity.map(::toDto)
-    fun toLiveEntity(dto: LiveData<Dto>) : LiveData<Entity> = Transformations.map(dto, ::toEntity)
-    fun toLiveDto(entity: LiveData<Entity>) : LiveData<Dto> = Transformations.map(entity, ::toDto)
-    fun toLiveListEntity(dto: LiveData<List<Dto>>) : LiveData<List<Entity>> = Transformations.map(dto, ::toListEntity)
-    fun toLiveListDto(entity: LiveData<List<Entity>>) : LiveData<List<Dto>> = Transformations.map(entity, ::toListDto)
+
+    fun toListEntity(dto: List<Dto>) : List<Entity> {
+        return dto.map(::toEntity)
+    }
+
+    fun toListDto(entity: List<Entity>) : List<Dto>{
+        return entity.map(::toDto)
+    }
+
+    fun toLiveEntity(dto: LiveData<Dto>) : LiveData<Entity>{
+        return Transformations.map(dto, ::toEntity)
+    }
+
+    fun toLiveDto(entity: LiveData<Entity>) : LiveData<Dto>{
+        return Transformations.map(entity, ::toDto)
+    }
+
+    fun toLiveListEntity(dto: LiveData<List<Dto>>) : LiveData<List<Entity>>{
+        return Transformations.map(dto, ::toListEntity)
+    }
+
+    fun toLiveListDto(entity: LiveData<List<Entity>>) : LiveData<List<Dto>>{
+        return Transformations.map(entity, ::toListDto)
+    }
 
 }
 
