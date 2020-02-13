df2c8c9e9471e33f1ffc29ac0c0b7ec592c4595f
wolf <whlauppe@web.de>
2020-02-13 23:13:08 +0100

    broke it

--- app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/ApiServiceBuilder.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/ApiServiceBuilder.kt
@@ -1,6 +1,7 @@
 package de.psekochbuch.exzellenzkoch.datalayer.remote
 
 
+//import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
@@ -8,6 +9,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.io.IOException
@@ -57,7 +59,7 @@ class ApiServiceBuilder(firebaseToken:String?) {
                 .client(createHttpClient())
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
               //Brauche ich das doch, laut Jack Wharton ist das inzwischen depcrecated
-                // .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                //.addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .build()
         } else {
             retrofit = Retrofit.Builder().baseUrl(BASE_URL)
@@ -67,6 +69,12 @@ class ApiServiceBuilder(firebaseToken:String?) {
 
     private fun createHttpClient(): OkHttpClient {
         //var token = uss?.getIdToken(false)?.result?.token
+        val logging = HttpLoggingInterceptor()
+        // set your desired log level
+        // set your desired log level
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        // add logging as last interceptor
 
 
         return OkHttpClient().newBuilder().addInterceptor(object : Interceptor {
@@ -79,7 +87,8 @@ class ApiServiceBuilder(firebaseToken:String?) {
                 val newRequest: Request = builder.build()
                 return chain.proceed(newRequest)
             }
-        }).build()
+        }).addInterceptor(logging)
+            .build()
 
     }
 
