b4d079861b9f1223dd2bdb48ded244c4c33d3f92
wolf <whlauppe@web.de>
2020-02-14 20:52:39 +0100

    copied admin api

---
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/datalayer/remote/api/AdminApi.kt
@@ -0,0 +1,29 @@
+package de.psekochbuch.exzellenzkoch.datalayer.remote.api
+
+interface AdminApi {
+
+    /*
+@RequestMapping("api/admin")
+interface AdminApi {
+
+    @GetMapping("/reported/recipes")
+    @ResponseBody
+    fun getReportedPublicRecipe(@RequestParam("page") page:Int,
+                                @RequestParam("readCount") readCount:Int): List<PublicRecipeDto>?
+
+    @GetMapping("/reported/users")
+    @ResponseBody
+    fun getReportedUser(@RequestParam("page") page:Int,
+                                @RequestParam("readCount") readCount:Int): List<UserDto>?
+
+    @DeleteMapping("/reported/{recipeId}")
+    fun deReportPublicRecipe(@RequestParam("recipeId") recipeId:Int)
+
+    @DeleteMapping("/reported/{userId}")
+    fun deReportUser(@RequestParam("userId") userId:String)
+
+
+
+     */
+
+}
