770d0c53d5eca4435e6aa1be9375933f1b174f3a
Thomas Weidmann <T.A.Weidmann@web.de>
2020-02-16 22:27:56 +0100

    kleine veränderung an test

--- app/src/test/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/PrivateRecipeTest.kt
+++ app/src/test/java/de/psekochbuch/exzellenzkoch/domainlayer/domainentities/PrivateRecipeTest.kt
@@ -8,7 +8,7 @@ class PrivateRecipeTest{
     @Test
     fun test(){
         val recipe = PrivateRecipe(1,"Bergsteiger",
-            "2,3   Gramm köse#kapitel1  \n1-3Gramm rabarber\n2#kapitel2",listOf(),"prep","pics/NSFL",1,2, Date(),3
+            "2,3   Gramm köse#kapitel1#kapitel2  \n1-3Gramm rabarber\n2#kapitel2",listOf(),"prep","pics/NSFL",1,2, Date(),3
         )
 
         val con = recipe.convertToPublicRepipe(User("testuser"))
