42bd71b99f2e4a065f695f8e673190dcae6fef48
lalanator <lea.strauch@gmx.de>
2020-02-16 23:29:02 +0100

    javadoc fragmets

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/view/SearchWithTagsFragment.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/view/SearchWithTagsFragment.kt
@@ -9,6 +9,12 @@ import androidx.fragment.app.viewModels
 import de.psekochbuch.exzellenzkoch.InjectorUtils
 import de.psekochbuch.exzellenzkoch.userinterfacelayer.viewmodel.SearchWithTagsViewModel
 
+/**
+ * The Fragment class provides logic for binding the respective .xml layout file to the class
+ * and calls functions from the underlying ViewModel.
+ * The ViewModel is provided by the ViewModelFactory, which is called here.
+ * SearchWithTagsFragment will bind to the SearchWithTagsFragment which will be implemented.
+ */
 class SearchWithTagsFragment : Fragment() {
 
     override fun onCreateView(
