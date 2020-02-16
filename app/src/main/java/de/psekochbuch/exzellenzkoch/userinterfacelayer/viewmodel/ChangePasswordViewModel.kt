66e1a89cb80c20fac33fd1ce2a114a65c78b1afe
Magnus Bühler <Magnus.b.buehler@gmail.com>
2020-02-16 22:52:46 +0100

    ChangePW eingefügt

--- app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ChangePasswordViewModel.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/userinterfacelayer/viewmodel/ChangePasswordViewModel.kt
@@ -1,24 +1,48 @@
 package de.psekochbuch.exzellenzkoch.userinterfacelayer.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.psekochbuch.exzellenzkoch.datalayer.remote.service.AuthentificationImpl
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.User
 import de.psekochbuch.exzellenzkoch.domainlayer.interfaces.services.Authentification
+import kotlinx.coroutines.launch
 
-class ChangePasswordViewModel(authentification: Authentification) : ViewModel() {
+/**
+ * The ChangePasswordViewModel handles the information for the ChangePasswordFragment.
+ *
+ */
+class ChangePasswordViewModel(authentificationImpl: Authentification) : ViewModel() {
 
 
-    var authentification = authentification
-    var password: MutableLiveData<String>? = null
+    var authentification = authentificationImpl
+    var password: MutableLiveData<String>? = MutableLiveData()
     var user: User? = null
+
+
+    private val _errorLiveDataString = MutableLiveData<String?>()
+    /**
+     * Request a snackbar to display a string.
+     */
+    val errorLiveDataString: LiveData<String?>
+        get() = _errorLiveDataString
 
     /**
      * Changes the password from the current User. The new password ist passed via LiveData and given to firebase
      *
      */
     fun changePassword() {
-        //repo.pwEdit(password)
-
+        if(password?.value.isNullOrEmpty()){
+            return
+        }
+        viewModelScope.launch {
+            try {
+                AuthentificationImpl.pwEdit(password?.value!!){}
+            } catch (error: Error) {
+                _errorLiveDataString.value = error.message
+            }
+        }
     }
 
 
