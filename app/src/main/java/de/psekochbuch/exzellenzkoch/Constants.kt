46579034446057324a2d50dc7165a69f49fbff52
lalanator <lea.strauch@gmx.de>
2020-03-01 20:16:47 +0100

    hardcode pagesize to 300

--- app/src/main/java/de/psekochbuch/exzellenzkoch/Constants.kt
+++ app/src/main/java/de/psekochbuch/exzellenzkoch/Constants.kt
@@ -4,5 +4,5 @@ package de.psekochbuch.exzellenzkoch
 /**
  * Constants used throughout the app.
  */
-const val PAGE_SIZE = 6
+const val PAGE_SIZE = 300
 const val DEFAULT_IMAGE = "file:///android_asset/exampleimages/vegetables_lowcontrast.png"
