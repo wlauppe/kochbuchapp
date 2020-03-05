33f592f25885e161135f96efc7d4863b958ed817
Thomas Weidmann <uyfgx@student.kit.edu>
2020-03-05 13:32:12 +0100

    lastentest noch nicht fertig

---
+++ app/src/androidTest/java/de/psekochbuch/exzellenzkoch/lastentest/userCreateTest.kt
@@ -0,0 +1,41 @@
+package de.psekochbuch.exzellenzkoch.lastentest
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import com.google.firebase.FirebaseApp
+import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.PublicRecipeRepositoryImp
+import de.psekochbuch.exzellenzkoch.datalayer.remote.service.AuthenticationResult
+import de.psekochbuch.exzellenzkoch.datalayer.remote.service.AuthentificationImpl
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+
+class userCreateTest{
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun test(){
+        for (user:Int in 1..2) {
+            var authResult: AuthenticationResult? = null
+            val repo = PublicRecipeRepositoryImp.getInstance()
+            FirebaseApp.initializeApp(ApplicationProvider.getApplicationContext())
+            val latch = CountDownLatch(1)
+            val userId = "user" + user + "@test.de"
+            val pw = "123456"
+            AuthentificationImpl.register(userId, pw, userId, { t, r ->
+                authResult = r
+                repo.setToken(t)
+                latch.countDown()
+            })
+
+
+
+            latch.await()
+
+            Assert.assertEquals(authResult, AuthenticationResult.REGISTRATIONSUCCESS)
+        }
+    }
+}
