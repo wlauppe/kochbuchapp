190e18c61b9f97065ef840ee504c4cf970c6c9e7
wolf <whlauppe@web.de>
2020-03-06 13:14:32 +0100

    lastentest läuft

--- app/src/androidTest/java/de/psekochbuch/exzellenzkoch/lastentest/publicreciperepotest.kt
+++ app/src/androidTest/java/de/psekochbuch/exzellenzkoch/lastentest/publicreciperepotest.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.test.core.app.ApplicationProvider
 import com.google.firebase.FirebaseApp
 import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.PublicRecipeRepositoryImp
+import de.psekochbuch.exzellenzkoch.datalayer.remote.repository.UserRepositoryImp
 import de.psekochbuch.exzellenzkoch.datalayer.remote.service.AuthenticationResult
 import de.psekochbuch.exzellenzkoch.datalayer.remote.service.AuthentificationImpl
 import de.psekochbuch.exzellenzkoch.domainlayer.domainentities.IngredientAmount
@@ -34,26 +35,32 @@ class publicreciperepotest{
     fun getPublicRecipe() {
         var authResult: AuthenticationResult? = null
         val repo = PublicRecipeRepositoryImp.getInstance()
+        val userRepo = UserRepositoryImp.getInstance()
         FirebaseApp.initializeApp(ApplicationProvider.getApplicationContext())
         val latch = CountDownLatch(1)
         val randomStr=getRandomString(12)
+
         AuthentificationImpl.register("extraTestServer${randomStr}@test.de","123456","random${randomStr}"){ a, b->
             authResult = b
             repo.setToken(a)
+            userRepo.setToken(a)
             latch.countDown()
             }
 
         latch.await(2, TimeUnit.SECONDS)
 
         assertEquals(authResult, AuthenticationResult.REGISTRATIONSUCCESS)
+        runBlocking {
+            val token = userRepo.addUser("random${randomStr}")
+        }
+        //var fromrepo = repo.getPublicRecipe(2).blockingObserve()!!
 
-        var fromrepo = repo.getPublicRecipe(2).blockingObserve()!!
 
-        fromrepo.imgUrl = ""
+        //fromrepo.imgUrl = ""
 
-        fromrepo = PublicRecipe(1,"titel","ingredients",
+        val fromrepo = PublicRecipe(0,"titel","ingredients",
             listOf(IngredientChapter(2, "chapter",listOf(IngredientAmount("ingedient",2.0,"Einheit")))),
-            listOf("tag1","tag2"),"So macht man es","",1,2,User(""),
+            listOf("tag1","tag2"),"So macht man es","",1,2,User("random${randomStr}"),
             Date(System.currentTimeMillis()),3,4.0)
         fromrepo.user = User(AuthentificationImpl.getUserId())
 
@@ -64,7 +71,7 @@ class publicreciperepotest{
                 fromrepo.title = titlewithoutnumber + i.toString()
                 runBlocking { repo.publishRecipe(fromrepo) }
             }
-        } catch (e:Exception){}
+        } catch (e:Error){}
         finally {
             AuthentificationImpl.userDelete()
         }
@@ -83,3 +90,5 @@ private fun <T> LiveData<T>.blockingObserve(): T? {
     latch.await()
     return value
 }
+
+
